cc11c9d6a479ec8387b264f1c939ea41e7cf35c6
patrice <patrice@cloozi.com>
2017-09-25 21:08:07 +0200

    Fix rank error when poule have exempt team

--- public/partials/wp-fftt-public-rank.php
+++ public/partials/wp-fftt-public-rank.php
@@ -27,40 +27,57 @@ if(is_array($rank) && count($rank) > 0 ){
 		?><div class="<?php echo $this->wpfftt_css_prefix ?>nav <?php echo $this->wpfftt_css_prefix ?>nav-tabs"><?php
 		foreach ($poules as $poule){
 			?>
-			<a href="<?php echo $this->get_rank_link($poule['iddiv'], $poule['idpoule']) ?>" class="<?php echo $this->wpfftt_css_prefix ?>nav-link<?php echo ($poule['idpoule'] === $id_poule_active) ? ' active' : null; ?>">
+            <a href="<?php echo $this->get_rank_link($poule['iddiv'], $poule['idpoule']) ?>" class="<?php echo $this->wpfftt_css_prefix ?>nav-link<?php echo ($poule['idpoule'] === $id_poule_active) ? ' active' : null; ?>">
 				<?php echo $poule['libelle'] ?>
-			</a>
+            </a>
 			<?php
 		}
 		?></div><?php
 	}?>
-<table class="<?php echo $this->wpfftt_css_prefix ?>table <?php echo $this->wpfftt_css_prefix ?>table-responsive <?php echo $this->wpfftt_css_prefix ?>table-striped">
-	<thead>
-	<tr>
-		<th><?php _e('Team', 'wp-fftt') ?></th>
-		<th><?php _e('P', 'wp-fftt') ?></th>
-		<th><?php _e('pts', 'wp-fftt') ?></th>
-		<th><?php _e('W', 'wp-fftt') ?></th>
-		<th><?php _e('L', 'wp-fftt') ?></th>
-		<th><?php _e('R', 'wp-fftt') ?></th>
-		<th><?php _e('Club', 'wp-fftt') ?></th>
-	</tr>
-	</thead>
-<?php
-	foreach ($rank as $r){
-	?>
-		<tr>
-			<td><?php echo $r['equipe'] ?></td>
-			<td><?php echo $r['joue'] ?></td>
-			<td><?php echo $r['pts'] ?></td>
-			<td class="<?php echo $this->wpfftt_css_prefix ?>bg-success <?php echo $this->wpfftt_css_prefix ?>table-inverse"><?php echo $r['totvic'] ?></td>
-			<td class="<?php echo $this->wpfftt_css_prefix ?>bg-danger <?php echo $this->wpfftt_css_prefix ?>table-inverse"><?php echo $r['totdef'] ?></td>
-			<td class="<?php echo $this->wpfftt_css_prefix ?>text-center"><a href="<?php echo $this->get_results_match_link($poule['iddiv'],$id_poule) ?>">&#8693;</a></td>
-			<td class="<?php echo $this->wpfftt_css_prefix ?>text-right"><small><em><a class="<?php echo $this->wpfftt_css_prefix ?>text-muted" href="<?php echo $this->get_club_link($r['numero']) ?>"><?php echo $this->get_club_name($r['numero']) ?></em></small></a></td>
-		</tr>
+    <table class="<?php echo $this->wpfftt_css_prefix ?>table <?php echo $this->wpfftt_css_prefix ?>table-responsive <?php echo $this->wpfftt_css_prefix ?>table-striped">
+        <thead>
+        <tr>
+            <th><?php _e('Team', 'wp-fftt') ?></th>
+            <th><?php _e('P', 'wp-fftt') ?></th>
+            <th><?php _e('pts', 'wp-fftt') ?></th>
+            <th><?php _e('W', 'wp-fftt') ?></th>
+            <th><?php _e('L', 'wp-fftt') ?></th>
+            <th><?php _e('R', 'wp-fftt') ?></th>
+            <th><?php _e('Club', 'wp-fftt') ?></th>
+        </tr>
+        </thead>
 		<?php
-	}
-	?>
-</table>
+		if(is_array($rank)) {
+			foreach ( $rank as $r ) {
+				?>
+                <tr>
+                    <td><?php echo is_string($r['equipe']) ? $r['equipe'] : 'exempt' ?></td>
+                    <td><?php echo is_string($r['joue']) ? $r['joue'] : '0' ?></td>
+                    <td><?php echo is_string($r['pts']) ? $r['pts'] : '0' ?></td>
+                    <td class="<?php echo $this->wpfftt_css_prefix ?>bg-success <?php echo $this->wpfftt_css_prefix ?>table-inverse"><?php echo $r['totvic'] ?></td>
+                    <td class="<?php echo $this->wpfftt_css_prefix ?>bg-danger <?php echo $this->wpfftt_css_prefix ?>table-inverse"><?php echo $r['totdef'] ?></td>
+                    <td class="<?php echo $this->wpfftt_css_prefix ?>text-center"><a
+                                href="<?php echo $this->get_results_match_link( $poule['iddiv'], $id_poule ) ?>">&#8693;</a>
+                    </td>
+                    <td class="<?php echo $this->wpfftt_css_prefix ?>text-right">
+						<?php if(is_string($r['numero'])) { ?>
+                            <small><em><a class="<?php echo $this->wpfftt_css_prefix ?>text-muted"
+                                          href="<?php echo $this->get_club_link( $r['numero'] ) ?>"><?php echo $this->get_club_name( $r['numero'] ) ?>
+                                </em></small>
+                            </a>
+						<?php } ?>
+                    </td>
+                </tr>
+				<?php
+			}
+		}else{
+			?>
+            <tr>
+                <td colspan="7">&nbsp;</td>
+            </tr>
+			<?php
+		}
+		?>
+    </table>
 	<?php
 }
